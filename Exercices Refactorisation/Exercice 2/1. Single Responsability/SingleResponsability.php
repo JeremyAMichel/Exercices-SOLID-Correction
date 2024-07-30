2acbc6e3df4af8f03d0fdf95414a52a1e12c5cbc
JeremyAMichel <jeremy.a.michel@hotmail.fr>
2024-07-30 12:43:55 +0200

    Exercice 2, SRP

--- Exercices Refactorisation/Exercice 2/1. Single Responsability/SingleResponsability.php
+++ Exercices Refactorisation/Exercice 2/1. Single Responsability/SingleResponsability.php
@@ -2,7 +2,19 @@
 
 namespace SRPViolation;
 
-class Modem
+interface ConnectionInterface
+{
+    public function dial($pno);
+    public function hangup();
+}
+
+interface DataChannelInterface
+{
+    public function send($c);
+    public function receive();
+}
+
+class Connection implements ConnectionInterface
 {
     public function dial($pno)
     {
@@ -11,9 +23,12 @@ class Modem
 
     public function hangup()
     {
-        //  Implementing hangup() method.
+        // Implementing hangup() method.
     }
+}
 
+class DataChannel implements DataChannelInterface
+{
     public function send($c)
     {
         // Implementing send($c) method.
@@ -22,5 +37,37 @@ class Modem
     public function receive()
     {
         // Implementing receive() method.
+    }
+}
+
+class Modem
+{
+    private $connection;
+    private $dataChannel;
+
+    public function __construct(ConnectionInterface $connection, DataChannelInterface $dataChannel)
+    {
+        $this->connection = $connection;
+        $this->dataChannel = $dataChannel;
+    }
+
+    public function dial($pno)
+    {
+        $this->connection->dial($pno);
+    }
+
+    public function hangup()
+    {
+        $this->connection->hangup();
+    }
+
+    public function send($c)
+    {
+        $this->dataChannel->send($c);
+    }
+
+    public function receive()
+    {
+        $this->dataChannel->receive();
     }
 }
