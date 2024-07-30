7a698faa5b20c3533c4f239010168c8da4a1e52c
JeremyAMichel <jeremy.a.michel@hotmail.fr>
2024-07-30 12:05:10 +0200

    Exercice 1, OC

--- Exercices Refactorisation/Exercice 1/2. OpenClosed Principle (OCP)/OpenClose.php
+++ Exercices Refactorisation/Exercice 1/2. OpenClosed Principle (OCP)/OpenClose.php
@@ -2,43 +2,33 @@
 
 // TODO refactoriser le code pour respecter le principe OCP de la programmation SOLID
 
-class Vehicle 
+abstract class Vehicle 
 {
+    abstract public function turnOn();
     public function run(){}
 }
 
 class Motorcycle extends Vehicle
 {
-    
+    public function turnOn()
+    {
+        echo 'Turning on the motorcycle';
+    }
 }
 
 class Car extends Vehicle
 {
-    
+    public function turnOn()
+    {
+        echo 'Turning on the car';
+    }
 }
 
 class Driver 
 {
     public function drive(Vehicle $vehicle)
     {
-        if ($vehicle instanceof Motorcycle) {
-            $this->turnOnMotorcycle();
-        }       
-        
-        if ($vehicle instanceof Car) {
-            $this->turnOnCar();
-        }
-        
+        $vehicle->turnOn();
         $vehicle->run();
-    }
-    
-    private function turnOnCar()
-    {
-        echo 'Turning on the car';
-    }
-    
-    private function turnOnMotorcycle()
-    {
-        echo 'Turning on the motorcycle';
     }
 }
